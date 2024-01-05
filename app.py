f47448ff9135099159e699cec684a8ddb40aaf25
Miclea Madalina <madalina.miclea02@e-uvt.ro>
2024-01-05 10:56:04 +0200

    Generare animale din baza de date

--- app.py
+++ app.py
@@ -1,68 +1,98 @@
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, redirect, url_for, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-categories = [
-    {"name": "Pisicuță", "image": "cat-1.jpg", "description": "Pisicuță adorabilă.", "button_link": "/cat_page.html", "type": "Pisică", "age": "Pui", "location": "Oraș 1"},
-    {"name": "Cățel", "image": "dog-1.jpg", "description": "Cățel prietenos și jucăuș.", "button_link": "/dog_page.html", "type": "Câine", "age": "Pui", "location": "Oraș 2"},
-    {"name": "Pisică", "image": "cat-2.jpg", "description": "Pisică pufoasă", "button_link": "/cat_page.html", "type": "Pisică", "age": "Adult", "location": "Oraș 1"},
-    {"name": "Câine", "image": "dog-2.jpg", "description": "Câine loial și jucăuș", "button_link": "/dog_page.html", "type": "Câine", "age": "Adult", "location": "Oraș 2"},
-    {"name": "Papagal", "image": "parrot-1.jpg", "description": "Papagal colorat.", "button_link": "/dog_page.html", "type": "Papagal", "age": "Pui", "location": "Oraș 1"},
-    # ... alte categorii
-]
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Petify.db'
+db = SQLAlchemy(app)
+admin = Admin(app, name='Admin', template_mode='bootstrap3')
+migrate = Migrate(app, db)
 
-# Ruta pentru pagina de start
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    button_link = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+
+class AnimalForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    image = StringField('Image', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    button_link = StringField('Button Link', validators=[DataRequired()])
+    type = StringField('Type', validators=[DataRequired()])
+    age = StringField('Age', validators=[DataRequired()])
+    location = StringField('Location', validators=[DataRequired()])
+
+@app.route('/add_animal', methods=['GET', 'POST'])
+def add_animal():
+    form = AnimalForm()
+    animals = Animal.query.all()
+
+    if form.validate_on_submit():
+        new_animal = Animal(
+            name=form.name.data,
+            image=form.image.data,
+            description=form.description.data,
+            button_link=form.button_link.data,
+            type=form.type.data,
+            age=form.age.data,
+            location=form.location.data
+        )
+        db.session.add(new_animal)
+        db.session.commit()
+        return redirect(url_for('add_animal'))
+
+    return render_template('add_animal.html', form=form, animals=animals)
+
 @app.route('/')
 def index():
-    return render_template('index.html', categories=categories)
+    animals = Animal.query.all()
+    print(animals)
+    return render_template('index.html', animals=animals)
 
-# Ruta dinamică pentru fiecare categorie de animal
 @app.route('/<category_name>')
 def category_page(category_name):
-    # Găsește categoria corespunzătoare
-    category = next((c for c in categories if c['name'].lower() == category_name.lower()), None)
+    category = Animal.query.filter_by(name=category_name).first()
     if category:
         return render_template('category_page.html', category=category)
-    else:
-        # Poți trata cazul în care categoria nu există, de exemplu, afișând o pagină de eroare sau redirectând către altă pagină
-        return render_template('category_not_found.html', category_name=category_name)
-            
+    #else:
+        #return render_template('category_not_found.html', category_name=category_name)
+
+
 @app.route('/search_results')
 def search_results():
     try:
         animal_type = request.args.get('animal_type')
         age = request.args.get('age')
         location = request.args.get('location')
-        print(f'Argumente: ',{animal_type}, {age}, {location})
-        
-        # Filtrare după parametrii de căutare și obținere de rezultate din lista de animale
-        filtered_animals = [animal for animal in categories if
-                            (not animal_type or animal.get('type') == animal_type) and
-                            (not age or animal.get('age') == age) and
-                            (not location or animal.get('location') == location)]
-        
-        filtering_results = []
-        for animal in categories:
-            if((not animal_type or animal.get("type") == animal_type)
-             and (not age or animal.get("age") == age)
-             and (not location or animal.get("location") == location)):
-                filtering_results.append(animal)
 
-        
-        for el in filtering_results:
-            print(f'la filtrare apare: ', el)
-        
-        return render_template('index.html', categories=filtering_results)
+        filtering_results = Animal.query.filter(
+            (not animal_type or Animal.type == animal_type) &
+            (not age or Animal.age == age) &
+            (not location or Animal.location == location)
+        ).all()
+
+        return render_template('index.html', animals=filtering_results)
     except Exception as e:
-        # Afișează detalii despre eroare în consolă
         print(str(e))
-        # Oferă un răspuns HTTP de eroare 500
         abort(500)
 
 @app.route('/index.html')
 def index2():
-    return render_template('index.html', categories=categories)
+    animals = Animal.query.all()
+    print(animals)
+    return render_template('index.html', animals=animals)
 
 @app.route('/about.html')
 def about():
@@ -118,5 +148,8 @@ def error():
     return render_template('404.html')
  """
 
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    button_link = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+
